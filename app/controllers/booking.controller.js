99b37094af009b2f6619dd280843c021b74e58d5
Piyush chourey <piyush.chourey@ideavate.com>
2022-05-21 01:11:22 +0530

    updated

--- app/controllers/booking.controller.js
+++ app/controllers/booking.controller.js
@@ -15,10 +15,18 @@ const create = async (req, res) => {
 			//documents upload functionality..
 			var bookingPostFilteredData = {aadharcardDoc :null, salarySlipDoc:null, agreementDoc:null};
 			var bookingPostFilteredData1 = _.pick(req.body, _.keys(bookingPostFilteredData));
-			await uploadImage(bookingPostFilteredData1).then(imageUploadArr => {
-				console.log(imageUploadArr)
-			})
-			console.log(uploadDocObj);
+			// let filename = await uploadImage(bookingPostFilteredData1);
+			
+			let aadharcarddoc =  sigleUploadImage({documents:bookingPostFilteredData1.aadharcardDoc});
+			let salarySlipDoc =  sigleUploadImage({documents:bookingPostFilteredData1.salarySlipDoc});
+
+			let agreementDoc =  sigleUploadImage({documents:bookingPostFilteredData1.agreementDoc});
+			let newArray = await Promise.all([aadharcarddoc,salarySlipDoc,agreementDoc]);
+			console.log(newArray)
+			bookingPostData['aadharcardDoc'] = newArray[0];
+			bookingPostData['salarySlipDoc'] = newArray[1];
+			bookingPostData['agreementDoc'] = newArray[2];
+			// return false;
 			Booking.create(bookingPostData).then(bookingData => {
 				res.send({ status:1, data:[], message: "Plot booked successfully!" });
 			}).catch(err => {
@@ -89,14 +97,42 @@ const uploadImage = async (req, res, callback) => {
 		try {
 			fs.writeFileSync("./images/" + fileName, imageBuffer, 'utf8');
 			response[key] = fileName;
-			console.log(req.length);
-			if(response.length == req.length){
-				return callback(response);
+			
+			// console.log(req, response);
+			console.log(Object.keys(req), Object.keys(response) )
+			if(Object.keys(req).length == Object.keys(response).length ){
+				console.log("final check")
+				return response;
 			}
 		} catch (e) {
 			callback(e); 
 		}
 	});
+}
+
+const sigleUploadImage = async (req, res, next) => {
+	// to declare some path to store your converted image
+	var matches = req.documents.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
+	response = {};
+	 
+	if (matches.length !== 3) {
+	return new Error('Invalid input string');
+	}
+	 
+	response.type = matches[1];
+	response.data = new Buffer(matches[2], 'base64');
+	let decodedImg = response;
+	let imageBuffer = decodedImg.data;
+	let type = decodedImg.type;
+	let extension = mime.getExtension(type); 
+	let randomName = new Date().getTime();
+	let fileName = randomName +"." + extension;
+	try {
+		fs.writeFileSync("./images/" + fileName, imageBuffer, 'utf8');
+		return fileName;
+	} catch (e) {
+		next(e); 
+	}
 }
 
 module.exports = {
